789432c8994736e598a778f0856eae826110bc04
Bhagyashree MR <bandebhagyashree@gmail.com>
2023-10-26 13:04:47 +0530

    Added test cases

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    """Create a test client for the Flask application."""
+    return app.test_client()
+
+def test_main_route(client):
+    """Test the main route."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Welcome!' in response.data
+
+def test_hello_route(client):
+    """Test the hello route."""
+    response = client.get('/how are you')
+    assert response.status_code == 200
+    assert b'I am good, how about you?' in response.data
