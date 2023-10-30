0f2b81ab8a1abfa8f71950c3ae97f1fcd02949ab
Bhagyashree MR <101582503+bhagyashree-mr@users.noreply.github.com>
2023-10-30 12:17:17 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,8 +18,10 @@
 #     assert response.status_code == 200
 #     assert b'I am good, how about you?' in response.data
 
+# tests/test_app.py
+from DEMOWEBAPP.app.app import app  # Assuming your Flask app is defined in app.py
+
 import pytest
-from app import app
 
 @pytest.fixture
 def client():
@@ -30,11 +32,12 @@ def test_main_route(client):
     """Test the main route."""
     response = client.get('/')
     assert response.status_code == 200
-    assert 'Welcome!' in response.data.decode('utf-8')
+    assert b'Welcome!' in response.data
 
 def test_hello_route(client):
     """Test the hello route."""
-    response = client.get('/how%20are%20you')  # Use URL-encoded format for spaces
+    response = client.get('/how are you')
     assert response.status_code == 200
-    assert 'I am good, how about you?' in response.data.decode('utf-8')
+    assert b'I am good, how about you?' in response.data
+
 
