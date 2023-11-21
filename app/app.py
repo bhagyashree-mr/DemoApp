7388d256d93131de5158e0a3b1bb637f06a1ad2b
Bhagyashree MR <101582503+bhagyashree-mr@users.noreply.github.com>
2023-11-21 13:13:24 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,5 +11,5 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8025)
